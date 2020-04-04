4a3fe4e43809b5fa4abfb7244413fd6f2955bfd6
Guilherme Yabu <guilhermeyabu@MacBook-Pro-de-Guilherme.local>
2020-04-04 18:58:53 -0300

    Aula 04: uso do nunjucks, refatoração e otimização, uso de layout fixo e de blocks para definir layout, criação de server para rodar nosso website

--- Workshop/server.js
+++ Workshop/server.js
@@ -3,13 +3,63 @@ const express = require("express");
 const server = express();
 
 //config arquivos estáticos como css, scripts, imagens
+server.use(express.static("public"));
 
-server.use(express.static("front-end"))
+const ideas = [
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+        title: "Cursos de Programação",
+        category: "Estudo",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod laboriosam assumenda laudantiu",
+        url: "https://rocketseat.com.br"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
+        title: "Exercícios",
+        category: "Saúde",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod laboriosam assumenda laudantiu",
+        url: "https://rocketseat.com.br"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
+        title: "Meditação",
+        category: "Mentalidade",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod laboriosam assumenda laudantiu",
+        url: "https://rocketseat.com.br"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
+        title: "Karaokê",
+        category: "Diversão em família",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod laboriosam assumenda laudantiu",
+        url: "https://rocketseat.com.br"
+    },
+]
+
+//config nunjuncks - permite usar variáveis no html.
+const nunjucks = require("nunjucks");
+nunjucks.configure("views", {
+    express: server,
+    noCache: true,
+})
 
 // criação de rota
 server.get("/", function(req, res) {
-    return res.sendFile(__dirname + "/front-end/index.html")
+    const reversedIdeas = [...ideas].reverse()
+    let lastIdeas = [];
+    for (let idea of reversedIdeas) {
+        if (lastIdeas.length < 2) {
+            lastIdeas.push(idea);
+        } 
+    }
+
+    return res.render("index.html", { ideas: lastIdeas });
+})
+
+server.get("/ideias", function(req, res) {
+    const reversedIdeas = [...ideas].reverse()
+    return res.render("ideias.html", { ideas: reversedIdeas });
 })
 
 // server escutando na porta 3000
-server.listen(3000)
+server.listen(3000) 
