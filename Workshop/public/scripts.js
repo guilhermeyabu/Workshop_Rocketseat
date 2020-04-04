3b88e5d603c41957a295f0c658f8fec52727abfc
Guilherme Yabu <guilhermeyabu@MacBook-Pro-de-Guilherme.local>
2020-04-04 20:13:55 -0300

    Aula 05: Adicionado banco de dados e inserindo ideias no banco e atualizando tela

--- Workshop/public/scripts.js
+++ Workshop/public/scripts.js
@@ -3,3 +3,27 @@ function onOff() {
     document.querySelector("body").classList.toggle("hideScroll")
     document.querySelector("#modal").classList.toggle("addScroll")
 }
+
+function checkFields() {
+    const valuesToCheck = [
+        "title", 
+        "image",
+        "category",
+        "description",
+        "link"
+    ]
+
+    const isEmpty =  valuesToCheck.find(function(value) {
+        const isString = typeof event.target[value].value === "string";
+        const isNotEmpty = !event.target[value].value.trim();
+        if (isString && isNotEmpty) {
+            return true;
+        }
+    })
+
+    if (isEmpty) {
+        event.preventDefault();
+        alert("Preencha todos os campos!");
+    }
+
+}
--- Workshop/server.js
+++ Workshop/server.js
@@ -1,40 +1,44 @@
 // uso do express para criar e configurar server
 const express = require("express");
 const server = express();
+const db = require("./db");
 
 //config arquivos estáticos como css, scripts, imagens
 server.use(express.static("public"));
 
-const ideas = [
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-        title: "Cursos de Programação",
-        category: "Estudo",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod laboriosam assumenda laudantiu",
-        url: "https://rocketseat.com.br"
-    },
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
-        title: "Exercícios",
-        category: "Saúde",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod laboriosam assumenda laudantiu",
-        url: "https://rocketseat.com.br"
-    },
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
-        title: "Meditação",
-        category: "Mentalidade",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod laboriosam assumenda laudantiu",
-        url: "https://rocketseat.com.br"
-    },
-    {
-        img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
-        title: "Karaokê",
-        category: "Diversão em família",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod laboriosam assumenda laudantiu",
-        url: "https://rocketseat.com.br"
-    },
-]
+//habilitar uso do req.body
+server.use(express.urlencoded({ extended: true }));
+
+// const ideas = [
+//     {
+//         img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+//         title: "Cursos de Programação",
+//         category: "Estudo",
+//         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod laboriosam assumenda laudantiu",
+//         url: "https://rocketseat.com.br"
+//     },
+//     {
+//         img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
+//         title: "Exercícios",
+//         category: "Saúde",
+//         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod laboriosam assumenda laudantiu",
+//         url: "https://rocketseat.com.br"
+//     },
+//     {
+//         img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
+//         title: "Meditação",
+//         category: "Mentalidade",
+//         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod laboriosam assumenda laudantiu",
+//         url: "https://rocketseat.com.br"
+//     },
+//     {
+//         img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
+//         title: "Karaokê",
+//         category: "Diversão em família",
+//         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod laboriosam assumenda laudantiu",
+//         url: "https://rocketseat.com.br"
+//     },
+// ]
 
 //config nunjuncks - permite usar variáveis no html.
 const nunjucks = require("nunjucks");
@@ -44,22 +48,98 @@ nunjucks.configure("views", {
 })
 
 // criação de rota
-server.get("/", function(req, res) {
-    const reversedIdeas = [...ideas].reverse()
-    let lastIdeas = [];
-    for (let idea of reversedIdeas) {
-        if (lastIdeas.length < 2) {
-            lastIdeas.push(idea);
+server.get("/", function (req, res) {
+
+    db.all(`SELECT * FROM ideas`, function (err, rows) {
+        if (err) {
+            console.log(err);
+            return res.send("Database error");
+        }
+
+        const reversedIdeas = [...rows].reverse()
+        let lastIdeas = [];
+        for (let idea of reversedIdeas) {
+            if (lastIdeas.length < 2) {
+                lastIdeas.push(idea);
+            }
+        }
+
+        return res.render("index.html", { ideas: lastIdeas });
+    });
+
+
+})
+
+server.get("/ideias", function (req, res) {
+
+    db.all(`SELECT * FROM ideas`, function (err, rows) {
+        if (err) {
+            console.log(err);
+            return res.send("Database error");
+        }
+
+        const reversedIdeas = [...rows].reverse()
+        return res.render("ideias.html", { ideas: reversedIdeas });
+    });
+})
+
+server.post("/", function(req, res) {
+    //Inserir dados
+    const query = `
+    INSERT INTO ideas(
+        image,
+        title,
+        category,
+        description,
+        link
+    ) VALUES (?,?,?,?,?);`
+
+    const values = [
+        req.body.image,
+        req.body.title,
+        req.body.category,
+        req.body.description,
+        req.body.link,
+    ]
+
+    db.run(query, values, function(err) {
+        if (err) {
+            console.log(err);
+            return res.send("Database Error");
         } 
-    }
 
-    return res.render("index.html", { ideas: lastIdeas });
-})
+        return res.redirect("/ideias")
+    });
+});
 
-server.get("/ideias", function(req, res) {
-    const reversedIdeas = [...ideas].reverse()
-    return res.render("ideias.html", { ideas: reversedIdeas });
-})
+server.post("/ideias", function(req, res) {
+    //Inserir dados
+    const query = `
+    INSERT INTO ideas(
+        image,
+        title,
+        category,
+        description,
+        link
+    ) VALUES (?,?,?,?,?);`
+
+    const values = [
+        req.body.image,
+        req.body.title,
+        req.body.category,
+        req.body.description,
+        req.body.link,
+    ]
+
+    db.run(query, values, function(err) {
+        if (err) {
+            console.log(err);
+            return res.send("Database Error");
+        } 
+
+        return res.redirect("/ideias")
+    });
+});
 
 // server escutando na porta 3000
 server.listen(3000) 
